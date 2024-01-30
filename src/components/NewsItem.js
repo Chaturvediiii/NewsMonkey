6d4540d039ac6d6e16d2926dfc628bafd907b242
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-01-30 14:03:04 +0530

    listen

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,23 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
-const NewsItem = (props) => {
-    let { title, description, imageURL, newsURL, author,publishedAt, } = props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img src={!imageURL?"https://img.freepik.com/free-vector/breaking-news-information-concept_52683-36243.jpg?size=626&ext=jpg&ga=GA1.2.309275571.1706365459&semt=sph":imageURL} className="card-img-top" alt="..." width={200} height={250} />
-          <div className="card-body">
-            <h5 className="card-title">{title}..</h5>
-            <p className="card-text">{description}..</p>
-            <p className="text-sm">By {!author?"Unknown":author}.. on {publishedAt} </p>
-            <Link to={newsURL} target="_blank" className="btn btn-dark btn-sm btn-primary">
-              Read More
-            </Link>
+function NewsItem(props) {
+  const [isListening, setIsListening] = useState(false);
+  const [utterance, setUtterance] = useState(null);
+
+  const handleListen = () => {
+    if (isListening) {
+      window.speechSynthesis.cancel();
+      setIsListening(false);
+    } else {
+      const newUtterance = new SpeechSynthesisUtterance();
+      newUtterance.text = `${props.title}. ${props.description}`;
+      window.speechSynthesis.speak(newUtterance);
+      setUtterance(newUtterance);
+      setIsListening(true);
+    }
+  };
+
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={
+            !props.imageURL
+              ? "https://media.wired.com/photos/659dbf12156f76cbf3057921/191:100/w_1280,c_limit/Gary-Gensler-sec-GettyImages-1660268020.jpg"
+              : props.imageURL
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}...</h5>
+          <p className="card-text">{props.description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary text-dark">
+              By {props.author} on {new Date(props.date).toTimeString()}
+            </small>
+          </p>
+          <div className="d-flex justify-content-between">
+          <button
+            onClick={handleListen}
+            className={`btn btn-sm ${isListening ? "btn-danger" : "btn-dark"}`}
+          >
+            {isListening ? "Stop Listening" : "Listen"}
+          </button>
+          <button
+            href={props.newsURL}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-dark ml-2"
+          >
+            Read more
+          </button>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default NewsItem
+export default NewsItem;
